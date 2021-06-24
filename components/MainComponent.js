60190f90e2db084daadfcdead41045a4abab3502
prettysmart1 <chelliemitchell@yahoo.com>
2021-06-24 17:32:12 -0400

    Redux Form Screen Drawer

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -14,6 +14,7 @@ import SafeAreaView from 'react-native-safe-area-view';
 import { connect } from 'react-redux';
 import { fetchCampsites, fetchComments, fetchPromotions,
     fetchPartners } from '../redux/ActionCreators';
+import Reservation from './ReservationComponent';
     
 const mapDispatchToProps = {
       fetchCampsites,
@@ -120,6 +121,29 @@ const HomeNavigator = createStackNavigator(
     }
 );
 
+const ReservationNavigator = createStackNavigator(
+  {
+      Reservation: { screen: Reservation }
+  },
+  {
+      defaultNavigationOptions: ({navigation}) => ({
+          headerStyle: {
+              backgroundColor: '#5637DD'
+          },
+          headerTintColor: '#fff',
+          headerTitleStyle: {
+              color: '#fff'
+          },
+          headerLeft: <Icon
+              name='tree'
+              type='font-awesome'
+              iconStyle={styles.stackIcon}
+              onPress={() => navigation.toggleDrawer()}
+          />
+      })
+  }
+);
+
 const CustomDrawerContentComponent = props => (
   <ScrollView>
       <SafeAreaView 
@@ -165,6 +189,20 @@ const MainNavigator = createDrawerNavigator(
                 />
             )
         }
+    },
+    Reservation: {
+      screen: ReservationNavigator,
+      navigationOptions: {
+          drawerLabel: 'Reserve Campsite',
+          drawerIcon: ({tintColor}) => (
+              <Icon
+                  name='tree'
+                  type='font-awesome'
+                  size={24}
+                  color={tintColor}
+              />
+          )
+      }
     },
     About: {
         screen: AboutNavigator,
